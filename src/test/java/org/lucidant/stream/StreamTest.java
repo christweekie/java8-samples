2a75ba6d2831c5caca1c3fa7b3c659738cb5c25f
Chris Faulkner <chris.faulkner@gmail.com>
2019-10-25 12:01:21 +0100

    Updates for Streams

--- src/test/java/org/lucidant/stream/StreamTest.java
+++ src/test/java/org/lucidant/stream/StreamTest.java
@@ -3,13 +3,17 @@ package org.lucidant.stream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import static java.util.Comparator.comparing;
 import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.maxBy;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
@@ -17,22 +21,38 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
  */
 class StreamTest
 {
+    final List<Transaction> transactions = new ArrayList<>();
 
-    @DisplayName("Groups into a map. Shows a grouping by.")
+    @BeforeEach
+    void beforeEach()
+    {
+        transactions.add(new Transaction(200, "Dave"));
+        transactions.add(new Transaction(499, "Dave"));
+        transactions.add(new Transaction(210, "Chris"));
+        transactions.add(new Transaction(199, "Chris"));
+    }
+
+    @DisplayName("Groups to Map. Shows a grouping by from Collectors.")
     @Test
     void testGroupToMap()
     {
-        final List<Transaction> txns = new ArrayList<>();
-        txns.add(new Transaction(200, "Dave"));
-        txns.add(new Transaction(499, "Dave"));
-        txns.add(new Transaction(210, "Chris"));
-        txns.add(new Transaction(199, "Chris"));
-
-        final Map<String, List<Transaction>> bigTxnbySeller = txns.stream()
+        final Map<String, List<Transaction>> largeBySeller = transactions.stream()
                                                              .filter(t -> t.getAmount() > 200)
                                                              .collect(groupingBy(Transaction::getPerson));
 
-        assertEquals(2, bigTxnbySeller.size());
+        assertEquals(2, largeBySeller.size());
+    }
+
+    @DisplayName("Groups to Map. Shows a grouping by from Collectors and a reduction to max.")
+    @Test
+    void testGroupToMapReducingToMax()
+    {
+        final Map<String, Optional<Transaction>> largestBySeller = transactions.stream()
+                                                                 .filter(t -> t.getAmount() > 200)
+                                                                 .collect(groupingBy(Transaction::getPerson,
+                                                                                     maxBy(comparing(Transaction::getAmount))));
+
+        assertEquals(2, largestBySeller.size());
     }
 
     @Getter
